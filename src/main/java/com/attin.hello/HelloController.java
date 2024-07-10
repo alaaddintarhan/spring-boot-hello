b22bb408eb2cdcd5af48bda2d0f6dd0a0eda2e3e
Alaaddin Tarhan <alaaddintarhan26@gmail.com>
2024-07-10 13:00:17 +0300

    usr & pwd

--- src/main/java/com/attin.hello/HelloController.java
+++ src/main/java/com/attin.hello/HelloController.java
@@ -1,5 +1,6 @@
 package com.attin.hello;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HelloController {
 
+    @Value("${service.token.username}")
+    private String username;
+
+    @Value("${service.token.password}")
+    private String password;
+
     @RequestMapping("/hi")
     String hello() {
         return "Hello World, Spring Boot!";
+    }
+
+    @RequestMapping("/usrpsw")
+    String usrpsw() {
+        return  "UserName :".concat(username)
+                .concat(" , password : ").concat(password);
     }
 }
